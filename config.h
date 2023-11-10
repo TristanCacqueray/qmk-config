339d7221eaee14c7269cb64ba23099e8d991d968
Tristan Cacqueray <tdecacqu@redhat.com>
2023-11-10 15:42:56 +0000

    Ensure custom shift works with LT

--- config.h
+++ config.h
@@ -13,8 +13,14 @@
 #define NO_MUSIC_MODE
 
 // tapping adjustment
-#define TAPPING_TERM 200
+#define TAPPING_TERM 190
 #define PERMISSIVE_HOLD
+// #define TAPPING_TERM_PER_KEY
+// #define QUICK_TAP_TERM_PER_KEY
 
 // toggle after one tap, like the default tap/toggle on oryx
 #define TAPPING_TOGGLE 1
+
+// Unfortunately, some applications drop or misorder fast key events. This is a
+// partial fix to slow down the rate at which macros are sent.
+#define TAP_CODE_DELAY 12
