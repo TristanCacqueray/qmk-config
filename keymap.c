13853c151f402073ef21e564db9e7a791dd1769b
Tristan Cacqueray <tdecacqu@redhat.com>
2023-11-11 16:01:30 +0000

    Move fun toggle to s/l

--- keymap.c
+++ keymap.c
@@ -25,8 +25,8 @@ const custom_shift_key_t custom_shift_keys[] = {
   {LALT_T(KC_F), LSFT(KC_F)}, // Shift f is F
   {RALT_T(KC_J), LSFT(KC_J)}, // Shift j is J
   {RCTL_T(KC_K), LSFT(KC_K)}, // Shift k is K
-  {LT(_FL, KC_V), LSFT(KC_V)},
-  {LT(_FL, KC_M), LSFT(KC_M)},
+  {LT(_FL, KC_S), LSFT(KC_S)},
+  {LT(_FL, KC_L), LSFT(KC_L)},
 };
 uint8_t NUM_CUSTOM_SHIFT_KEYS =
     sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_moonlander(
     xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,                xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
     TG(_ML),        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           xxxxxxxx,                xxxxxxxx,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_GRAVE,
-    KC_TAB,         KC_A,           KC_S,           LCTL_T(KC_D),   LALT_T(KC_F),   KC_G,           xxxxxxxx,                xxxxxxxx,       KC_H,           RALT_T(KC_J),   RCTL_T(KC_K),   KC_L,           KC_SEMICOLON,   KC_QUOTE,
-    KC_ESCAPE,      KC_Z,           KC_X,           KC_C,           LT(_FL, KC_V),  KC_B,                                                    KC_N,           LT(_FL, KC_M),  KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
+    KC_TAB,         KC_A,           LT(_FL, KC_S),  LCTL_T(KC_D),   LALT_T(KC_F),   KC_G,           xxxxxxxx,                xxxxxxxx,       KC_H,           RALT_T(KC_J),   RCTL_T(KC_K),   LT(_FL, KC_L),  KC_SEMICOLON,   KC_QUOTE,
+    KC_ESCAPE,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
     xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       KC_LGUI,        KC_LEFT_SHIFT,                  xxxxxxxx,                xxxxxxxx,                       TT(_CL),        KC_MINUS,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
                                                                                     KC_BSPC,            KC_NO,KC_NO,   KC_NO,KC_NO,          KC_SPACE
   ),
